3aabae4ccc8159d427d4718b01a428eadad11b77
mohammadhhhhhh <9025722@student.zadkine.nl>
2024-10-07 15:33:58 +0200

    Add files via upload

---
+++ week2/app.js
@@ -0,0 +1,16 @@
+let heading = document.getElementById('heading')
+
+function changetext() {
+heading.innerText = "goedeavond";
+heading.style.color = "red" ;
+heading.style.backgroundColor = "yellow"
+
+
+
+    
+}
+
+
+
+
+
